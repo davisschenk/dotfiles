cfd66547abcabf17a0f304b4a73dba94470f6c48
Davis Schenkenberger <davisschenk@gmail.com>
2023-08-30 09:16:57 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 FILES=$(git ls-files --cached | grep -E '^([^/]+|.*\/$)' -o | uniq)
 DIR=$(git rev-parse --show-toplevel)
 
-for file in $FILES;
-    do ln -sv "${DIR}/${file}" "~/${file}"
+for file in $FILES; do
+    sudo ln -sfv "${DIR}/${file}" "$HOME"
+    echo ${DIR} ${file}
 done;
